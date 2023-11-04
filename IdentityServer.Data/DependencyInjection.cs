04cd576c9a472c51537df6c81818e6152c641bf2
Alex <alex@hagl.de>
2023-11-04 11:45:47 +0100

    Added project

---
+++ IdentityServer.Data/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer.Data; 
+
+public static class DependencyInjection {
+
+    public static IServiceCollection AddDataServices(this IServiceCollection services, IConfiguration config) {
+        return services;
+    }
+    
+    
+}
